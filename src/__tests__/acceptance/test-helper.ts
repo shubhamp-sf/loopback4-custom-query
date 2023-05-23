7b5063f97e3bc2ca9869a4b1a6332bbdad3d516c
Shubham P <shubham.prajapat@sourcefuse.com>
2023-05-23 12:59:29 +0530

    lb4 scaffold

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {CustomQueryApp} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new CustomQueryApp({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: CustomQueryApp;
+  client: Client;
+}
